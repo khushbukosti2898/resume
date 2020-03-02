e65189353b9c95239873cf77c8dca7b2e1f0921c
bacancy <khushbu.kosti@bacancytechnology.com>
2020-03-02 13:08:58 +0530

    commit1

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,17 +9,18 @@ export default class Sidebar extends Component {
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
-              <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gmail.com</span>
+              <h1 id="colorlib-logo"><a href="index.html">Khushbu Koshti</a></h1>
+              <span className="email"><i className="icon-mail"></i> kostikhushbu1234@gmail.com</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
-                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  {/* <li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li> */}
                   <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                  <li><a href="#gallary" data-nav-section="gallary">Gallary</a></li>
                 </ul>
               </div>
             </nav>
